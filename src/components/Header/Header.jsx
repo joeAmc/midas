46f68f201c509003158bd693def2640ac5c8f974
joe mcilroy <joeamcilroy@gmail.com>
2022-07-12 09:41:38 +0100

    header image and div

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import "../Header/header.css";
+import Logo from "../../assets/Logo.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <img src={Logo} alt="" />
+    </div>
+  );
 };
 
 export default Header;
