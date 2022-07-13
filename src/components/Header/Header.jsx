b8fd0177d90f72e09df62eedf9bf7de322b9e27d
joe mcilroy <joeamcilroy@gmail.com>
2022-07-13 11:01:20 +0100

    header style and links added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+// import { Link } from "react-router-dom";
 import "../Header/header.css";
 import Logo from "../../assets/Logo.png";
 
@@ -6,6 +7,11 @@ const Header = () => {
   return (
     <div className="header">
       <img src={Logo} alt="" />
+      <div className="nav">
+        <a href="">About</a>
+        <a href="">Collections</a>
+        <a href="">Contact</a>
+      </div>
     </div>
   );
 };
