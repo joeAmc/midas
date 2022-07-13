aa5302cdf2f9e428d62372c0fd31cb9190d4db81
joe mcilroy <joeamcilroy@gmail.com>
2022-07-13 10:02:16 +0100

    routes and pages added, logo added to header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Header from "./components/Header/Header";
+import HomePage from "./pages/Home";
+import RingsPage from "./pages/Rings";
+import EarringsPage from "./pages/Earrings";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Route path="/rings">
+          <RingsPage />
+        </Route>
+        <Route path="/earrings">
+          <EarringsPage />
+        </Route>
+      </Switch>
       <h1>Midas Touch</h1>
     </div>
   );
