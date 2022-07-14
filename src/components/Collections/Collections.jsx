63736dfcfa6b29ab73dddb7d21a15c941065a75e
joe mcilroy <joeamcilroy@gmail.com>
2022-07-14 09:41:07 +0100

    collections and about component folders

---
+++ src/components/Collections/Collections.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Collections = () => {
+  return (
+    <section id="collections">
+      <div>Collections</div>
+    </section>
+  );
+};
+
+export default Collections;
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section id="contact">
+      <div>Contact</div>;
+    </section>
+  );
 };
 
 export default Contact;
