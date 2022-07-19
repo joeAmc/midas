d5d7db9a8ff1363ab12e5d97b1ef52885dfe839e
joe mcilroy <joeamcilroy@gmail.com>
2022-07-19 15:53:22 +0100

    collections images added

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "../Contact/contact.css";
 
 const About = () => {
   return (
     <section id="about">
       <div className="spacer"></div>
-      <div>About</div>
+      <div className="about-text"></div>
     </section>
   );
 };
--- src/components/Collections/Collections.jsx
+++ src/components/Collections/Collections.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import "../Collections/collections.css";
+import Ring from "../../assets/midas rings.jpg";
+import Earring from "../../assets/midas earrings.jpg";
 
 const Collections = () => {
   return (
     <section id="collections">
       <div className="spacer"></div>
-      <div>Collections</div>
+      <div className="rings">
+        <img src={Ring} alt="" />
+      </div>
+      <div className="earrings">
+        <img src={Earring} alt="" />
+      </div>
     </section>
   );
 };
